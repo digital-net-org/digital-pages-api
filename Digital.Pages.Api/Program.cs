f0e020c22027369183e5be497aa78d1e5cc1ca92
BenoitSafari <gregory.michalak.192@gmail.com>
2025-03-17 00:24:02 +0100

    feature(bootstrap): SDK Update

--- Digital.Pages.Api/Program.cs
+++ Digital.Pages.Api/Program.cs
@@ -2,18 +2,18 @@ using Digital.Lib.Net.Authentication;
 using Digital.Lib.Net.Files;
 using Digital.Lib.Net.Mvc;
 using Digital.Lib.Net.Sdk;
+using Digital.Lib.Net.Sdk.Services.Application;
 using Digital.Pages.Api.Data;
 
 namespace Digital.Pages.Api;
 
 public sealed class Program
 {
-    private const string AppName = "Digital.Pages.Api";
-
     private static async Task Main(string[] args)
     {
         var app = WebApplication.CreateBuilder(args)
-            .AddDigitalSdk(AppName)
+            .SetApplicationName("Digital.Pages.Api")
+            .AddDigitalSdk()
             .AddDigitalAuthentication()
             .AddDigitalFilesServices()
             .AddDigitalMvc()
@@ -21,7 +21,7 @@ public sealed class Program
             .Build();
 
         await app
-            .UseDigitalSdk(AppName)
+            .UseDigitalSdk()
             .RunAsync();
     }
 }
